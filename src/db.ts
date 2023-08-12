9b0504c0e87aa14514feb209a09627ca1071da73
ErmakovEv <ej-88@ya.ru>
2023-08-12 20:49:30 +0300

    deploy err7

--- src/db.ts
+++ src/db.ts
@@ -2,5 +2,5 @@ import { Sequelize } from 'sequelize';
 import constans from './constans';
 
 export default new Sequelize(
-  'postgres://admin:57NQbgvyZklIdux5MJ8YiNQ3DxxCqoU6@dpg-cjbj2g3bq8nc73d77p40-a.oregon-postgres.render.com/one_okjy'
+  'postgres://admin:57NQbgvyZklIdux5MJ8YiNQ3DxxCqoU6@dpg-cjbj2g3bq8nc73d77p40-a/one_okjy'
 );
