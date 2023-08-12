42cba8b836486a1ee85a9e321faa29af2b68ea9b
ErmakovEv <ej-88@ya.ru>
2023-08-12 21:30:12 +0300

    deploy err12

--- build/db.js
+++ build/db.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, '__esModule', { value: true });
 const sequelize_1 = require('sequelize');
 exports.default = new sequelize_1.Sequelize(
-  'postgres://admin:JmzYeKRACcOJCeP3pQK1PDWqzycPBZPu@dpg-cjbsp07db61s73ersbl0-a/dbname_4sy3'
+  'postgres://admin:JmzYeKRACcOJCeP3pQK1PDWqzycPBZPu@dpg-cjbsp07db61s73ersbl0-a/dbname_4sy3?ssl=true'
 );
 //# sourceMappingURL=db.js.map
