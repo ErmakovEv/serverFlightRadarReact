22aa5e30a5b4aa3bbef62e3fa71a62b10180e7bb
ErmakovEv <ej-88@ya.ru>
2023-08-12 21:20:53 +0300

    deploy err11

--- build/db.js
+++ build/db.js
@@ -1,5 +1,7 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("sequelize");
-exports.default = new sequelize_1.Sequelize('postgres://admin:JmzYeKRACcOJCeP3pQK1PDWqzycPBZPu@dpg-cjbsp07db61s73ersbl0-a.oregon-postgres.render.com/dbname_4sy3');
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+const sequelize_1 = require('sequelize');
+exports.default = new sequelize_1.Sequelize(
+  'postgres://admin:JmzYeKRACcOJCeP3pQK1PDWqzycPBZPu@dpg-cjbsp07db61s73ersbl0-a/dbname_4sy3'
+);
 //# sourceMappingURL=db.js.map
