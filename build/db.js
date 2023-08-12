c0280264e1197e77444462482365769f674c6f2a
ErmakovEv <ej-88@ya.ru>
2023-08-12 21:38:57 +0300

    deploy err18

--- build/db.js
+++ build/db.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, '__esModule', { value: true });
 const sequelize_1 = require('sequelize');
 exports.default = new sequelize_1.Sequelize(
   // 'postgres://admin:JmzYeKRACcOJCeP3pQK1PDWqzycPBZPu@dpg-cjbsp07db61s73ersbl0-a/dbname_4sy3'
-  'postgres://admin:JmzYeKRACcOJCeP3pQK1PDWqzycPBZPu@dpg-cjbsp07db61s73ersbl0-a.oregon-postgres.render.com/dbname_4sy3'
+  'postgres://admin:JmzYeKRACcOJCeP3pQK1PDWqzycPBZPu@dpg-cjbsp07db61s73ersbl0-a.oregon-postgres.render.com/dbname_4sy3?SSL=true'
 );
 //# sourceMappingURL=db.js.map
