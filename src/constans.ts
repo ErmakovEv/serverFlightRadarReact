080efae29defc61c3d0cb390adc6c06529ff7f0f
ErmakovEv <ej-88@ya.ru>
2023-08-12 10:19:41 +0300

    first

--- src/constans.ts
+++ src/constans.ts
@@ -2,7 +2,7 @@ export default {
   PORT: 3000,
   DB_NAME: 'one',
   // DB_NAME: 'aviaportal',
-  DB_USER: 'postgres',
+  DB_USER: 'admin',
   DB_PASS: '2109',
   DB_HOST: 'localhost',
   DB_PORT: 5432,
--- src/db.ts
+++ src/db.ts
@@ -1,8 +1,6 @@
 import { Sequelize } from 'sequelize';
 import constans from './constans';
 
-export default new Sequelize(constans.DB_NAME, constans.DB_USER, constans.DB_PASS, {
-  dialect: 'postgres',
-  host: constans.DB_HOST,
-  port: constans.DB_PORT,
-});
+export default new Sequelize(
+  'postgres://admin:57NQbgvyZklIdux5MJ8YiNQ3DxxCqoU6@dpg-cjbj2g3bq8nc73d77p40-a.oregon-postgres.render.com/one_okjy'
+);
