a657780d92c1d7259d91d4a99a40057d1286aa41
ErmakovEv <ej-88@ya.ru>
2023-08-12 20:46:02 +0300

    deploy err6

--- build/db.js
+++ build/db.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-exports.default = new sequelize_1.Sequelize('postgres://admin:57NQbgvyZklIdux5MJ8YiNQ3DxxCqoU6@dpg-cjbj2g3bq8nc73d77p40-a/one_okjy');
+exports.default = new sequelize_1.Sequelize('postgres://admin:57NQbgvyZklIdux5MJ8YiNQ3DxxCqoU6@dpg-cjbj2g3bq8nc73d77p40-a.oregon-postgres.render.com/one_okjy');
 //# sourceMappingURL=db.js.map
