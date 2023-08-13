abe3ab2022059abe47cba97ab2b8bbe12e55f3c2
ErmakovEv <ej-88@ya.ru>
2023-08-13 19:57:59 +0300

    deploy:cors settings

--- build/main.js
+++ build/main.js
@@ -1,36 +1,37 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const path = require("path");
-const constans_1 = require("./constans");
-const db_1 = require("./db");
-const cors = require("cors");
-const fileupload = require("express-fileupload");
-const cookieParser = require("cookie-parser");
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+const express = require('express');
+const path = require('path');
+const constans_1 = require('./constans');
+const db_1 = require('./db');
+const cors = require('cors');
+const fileupload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const models = require('./models/models'); //???
-const router_1 = require("./routes/router");
+const router_1 = require('./routes/router');
 const PORT = constans_1.default.PORT;
 const app = express();
 app.use(cookieParser());
-app.use(cors({
-    origin: 'http://127.0.0.1:5173',
+app.use(
+  cors({
+    origin: 'https://flightradarclone.onrender.com',
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization', 'cookie'],
-}));
+  })
+);
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'static')));
 app.use(fileupload());
 app.use('/api', router_1.default);
 const start = async () => {
-    try {
-        await db_1.default.authenticate();
-        await db_1.default.sync();
-        app.listen(PORT, () => console.log(`Server run on port ${PORT}...`));
-    }
-    catch (error) {
-        console.log(error);
-    }
+  try {
+    await db_1.default.authenticate();
+    await db_1.default.sync();
+    app.listen(PORT, () => console.log(`Server run on port ${PORT}...`));
+  } catch (error) {
+    console.log(error);
+  }
 };
 start();
 /*
