14fb71511e9a41107d5d8bc7f68c276e3db6731c
ErmakovEv <ej-88@ya.ru>
2023-09-03 22:17:15 +0300

    settings for db

--- src/constans.ts
+++ src/constans.ts
@@ -1,8 +1,9 @@
 export default {
   PORT: 3000,
-  DB_NAME: 'one',
+  DB_NAME: 'flight',
   // DB_NAME: 'aviaportal',
-  DB_USER: 'postgres',
+  // DB_USER: 'postgres',
+  DB_USER: 'user',
   DB_PASS: '2109',
   DB_HOST: 'localhost',
   DB_PORT: 5432,
--- src/db.ts
+++ src/db.ts
@@ -4,10 +4,15 @@ import * as dotenv from 'dotenv';
 
 let db: Sequelize;
 if (process.env.NODE_ENV === 'production') {
-  console.log('prod!!!');
-  db = new Sequelize(
-    'postgres://admin:4OTwdMv1TY6c0b9Ib8bKAtSmXrhhlfYC@dpg-cjcdiu45kgrc73amtgng-a.oregon-postgres.render.com/db_fr?ssl=true'
-  );
+  // console.log('prod!!!');
+  // db = new Sequelize(
+  //   'postgres://admin:4OTwdMv1TY6c0b9Ib8bKAtSmXrhhlfYC@dpg-cjcdiu45kgrc73amtgng-a.oregon-postgres.render.com/db_fr?ssl=true'
+  // );
+  db = new Sequelize(constans.DB_NAME, constans.DB_USER, constans.DB_PASS, {
+    dialect: 'postgres',
+    host: constans.DB_HOST,
+    port: constans.DB_PORT,
+  });
 } else {
   console.log('dev!!!');
   db = new Sequelize(constans.DB_NAME, constans.DB_USER, constans.DB_PASS, {
