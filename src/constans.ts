382a54c83296900a3d658e40bccc0635682f4ac7
ErmakovEv <ej-88@ya.ru>
2024-03-30 13:55:07 +0300

    change endpoint getFlights

--- src/constans.ts
+++ src/constans.ts
@@ -1,9 +1,8 @@
 export default {
   PORT: 3000,
-  DB_NAME: 'flight',
+  DB_NAME: 'one',
   // DB_NAME: 'aviaportal',
-  // DB_USER: 'postgres',
-  DB_USER: 'user',
+  DB_USER: 'postgres',
   DB_PASS: '2109',
   DB_HOST: 'localhost',
   DB_PORT: 5432,
--- src/controllers/proxyController.ts
+++ src/controllers/proxyController.ts
@@ -29,16 +29,13 @@ class FlightRadarApiController {
     const { bounds } = req.params;
 
     await https.get(
-      `https://data-live.flightradar24.com/zones/fcgi/feed.js?bounds=${bounds}&faa=1&satellite=1&mlat=1&flarm=1&adsb=1&gnd=0&air=1&vehicles=1&estimated=1&maxage=14400`,
+      `https://data-cloud.flightradar24.com/zones/fcgi/feed.js?bounds=${bounds}&faa=1&satellite=1&mlat=1&flarm=1&adsb=1&gnd=1&air=1&vehicles=1&estimated=1&maxage=14400`,
       responseHandler(res)
     );
   }
 
   async getAllAirports(req: express.Request, res: express.Response) {
-    await https.get(
-      'https://www.flightradar24.com/_json/airports.php',
-      responseHandler(res)
-    );
+    await https.get('https://www.flightradar24.com/_json/airports.php', responseHandler(res));
   }
 
   async getAirport(req: express.Request, res: express.Response) {
@@ -52,18 +49,12 @@ class FlightRadarApiController {
   async getFlightStatus(req: express.Request, res: express.Response) {
     const { id } = req.params;
     console.log(id);
-    await https.get(
-      `https://data-live.flightradar24.com/clickhandler/?version=1.5&flight=${id}`,
-      responseHandler(res)
-    );
+    await https.get(`https://data-live.flightradar24.com/clickhandler/?version=1.5&flight=${id}`, responseHandler(res));
   }
 
   async getShedule(req: express.Request, res: express.Response) {
     const { id } = req.params;
-    await https.get(
-      `https://api.flightradar24.com/common/v1/airport.json?code=${id}`,
-      responseHandler(res)
-    );
+    await https.get(`https://api.flightradar24.com/common/v1/airport.json?code=${id}`, responseHandler(res));
   }
 }
 
